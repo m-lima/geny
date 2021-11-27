4d3622cd43ef5a5b9bb953119e29adbe6240ef31
Marcelo Lima <marcelowind@gmail.com>
2021-11-27 10:16:53 +0100

    Possibly working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 mod being;
 mod geo;
 mod neural;
@@ -23,3 +25,52 @@ fn main() -> anyhow::Result<()> {
 
     Ok(())
 }
+
+// #[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Debug, Hash)]
+// enum Input {
+//     Random,
+//     DirectionVertical,
+//     DirectionHorizontal,
+// }
+
+// impl Input {
+//     fn spike(&self, world: &World, index: usize) -> signal::Half {
+//         use signal::Signal;
+
+//         match self {
+//             Self::Random => signal::Half::cap(rand::random::<f32>()),
+//             Self::DirectionVertical => match world.being(index).direction() {
+//                 Direction::East | Direction::West => signal::Half::cap(0.5),
+//                 Direction::North => signal::Half::cap(1.0),
+//                 Direction::South => signal::Half::cap(0.0),
+//             },
+//             Self::DirectionHorizontal => match world.being(index).direction() {
+//                 Direction::North | Direction::South => signal::Half::cap(0.5),
+//                 Direction::East => signal::Half::cap(1.0),
+//                 Direction::West => signal::Half::cap(0.0),
+//             },
+//         }
+//     }
+// }
+
+// #[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Debug, Hash)]
+// enum Output {
+//     Noop,
+//     TurnLeft,
+//     TurnRight,
+//     Advance,
+// }
+
+// impl Output {
+//     fn spike(&self, world: &mut World, index: usize) {
+//         match self {
+//             Self::Noop => {}
+//             Self::TurnLeft => world.being_mut(index).turn_left(),
+//             Self::TurnRight => world.being_mut(index).turn_right(),
+//             Self::Advance => {
+//                 let direction = world.being(index).direction();
+//                 world.advance(index, direction);
+//             }
+//         }
+//     }
+// }
--- src/neural.rs
+++ src/neural.rs
@@ -1,69 +1,116 @@
+use signal::{Aggregator, Amplifier, Signal};
+
 #[derive(Copy, Clone, Eq, PartialEq)]
-enum Visitability {
-    Unvisited,
-    Visiting,
-    Visited,
+enum Layer {
+    Input,
+    Internal,
+    Output,
 }
 
-struct Dentrite<'a, const A: u8> {
-    // TODO: This may cause problems. If that is the case, we can use a Vec arena index
-    axon: &'a mut Neuron<'a, A>,
-    synapse: signal::Amplifier<A>,
+#[derive(Copy, Clone, Eq, PartialEq)]
+struct Pointer {
+    layer: Layer,
+    neuron: usize,
 }
 
-struct Neuron<'a, const A: u8> {
-    dentrites: Vec<Dentrite<'a, A>>,
-    aggregator: signal::Aggregator,
-    latch: signal::Full,
-    visitability: Visitability,
+struct Dentrite<const A: u8> {
+    axon: Pointer,
+    synapse: Amplifier<A>,
 }
 
-impl<'a, const A: u8> Neuron<'a, A> {
-    fn update(&mut self) -> signal::Full {
-        if self.visitability == Visitability::Unvisited {
-            self.visitability = Visitability::Visiting;
+trait Input<State> {
+    fn update(&self, state: &State) -> Signal;
+}
 
-            self.latch = self.aggregator.aggregate(
-                self.dentrites
-                    .iter_mut()
-                    .map(|d| d.synapse * d.axon.update()),
-            );
+struct InputNeuron<State, In: Input<State>, const A: u8> {
+    input: In,
+    latch: Signal,
+    visited: bool,
+    _marker: std::marker::PhantomData<State>,
+}
 
-            self.visitability = Visitability::Visited;
+trait Output<State> {
+    fn update(&self, state: &mut State);
+}
+
+struct OutputNeuron<State, Out: Output<State>, const A: u8> {
+    out: Out,
+    dentrites: Vec<Dentrite<A>>,
+    aggregator: Aggregator,
+    _marker: std::marker::PhantomData<State>,
+}
+
+struct InternalNeuron<const A: u8> {
+    dentrites: Vec<Dentrite<A>>,
+    aggregator: Aggregator,
+    latch: Signal,
+    visited: bool,
+}
+
+struct Brain<State, In: Input<State>, Out: Output<State>, const A: u8> {
+    inputs: Vec<InputNeuron<State, In, A>>,
+    internals: Vec<InternalNeuron<A>>,
+    outputs: Vec<OutputNeuron<State, Out, A>>,
+}
+
+impl<State, In: Input<State>, Out: Output<State>, const A: u8> Brain<State, In, Out, A> {
+    fn update(&mut self, pointer: Pointer, state: &State) -> Signal {
+        match pointer.layer {
+            Layer::Input => {
+                let neuron = &mut self.inputs[pointer.neuron];
+                if !neuron.visited {
+                    neuron.visited = true;
+                    neuron.latch = neuron.input.update(state);
+                }
+                neuron.latch
+            }
+            Layer::Internal => {
+                let neuron: *mut InternalNeuron<A> = &mut self.internals[pointer.neuron];
+                unsafe {
+                    if !(*neuron).visited {
+                        (*neuron).visited = true;
+                        (*neuron).latch = (*neuron).aggregator.aggregate(
+                            (*neuron)
+                                .dentrites
+                                .iter()
+                                .map(|d| d.synapse * self.update(d.axon, state)),
+                        );
+                    }
+                    (*neuron).latch
+                }
+            }
+            Layer::Output => unreachable!("Output layer can never be a source of signal"),
         }
+    }
 
-        self.latch
+    pub fn step(&mut self, state: &mut State) {
+        for i in 0..self.outputs.len() {
+            let neuron: *const OutputNeuron<State, Out, A> = &self.outputs[i];
+            unsafe {
+                let probability = (*neuron).aggregator.aggregate(
+                    (*neuron)
+                        .dentrites
+                        .iter()
+                        .map(|d| d.synapse * self.update(d.axon, state)),
+                );
+                if rand::random::<f32>() < probability.as_f32() {
+                    (*neuron).out.update(state);
+                }
+            }
+        }
     }
 }
 
 mod signal {
-    pub trait Signal {
-        fn cap(value: f32) -> Self;
-        fn as_f32(&self) -> f32;
-    }
-
     #[derive(Copy, Clone, PartialEq, PartialOrd, Debug)]
-    pub struct Full(f32);
+    pub struct Signal(f32);
 
-    impl Signal for Full {
+    impl Signal {
         fn cap(value: f32) -> Self {
             Self(value.min(1.0).max(-1.0))
         }
 
-        fn as_f32(&self) -> f32 {
-            self.0
-        }
-    }
-
-    #[derive(Copy, Clone, PartialEq, PartialOrd, Debug)]
-    pub struct Half(f32);
-
-    impl Signal for Half {
-        fn cap(value: f32) -> Self {
-            Self(value.min(1.0).max(0.0))
-        }
-
-        fn as_f32(&self) -> f32 {
+        pub fn as_f32(self) -> f32 {
             self.0
         }
     }
@@ -71,25 +118,11 @@ mod signal {
     #[derive(Copy, Clone, PartialOrd, PartialEq, Debug)]
     pub struct Amplifier<const AMPLITUDE: u8>(f32);
 
-    impl<const AMPLITUDE: u8> std::ops::Mul<Full> for Amplifier<AMPLITUDE> {
-        type Output = Full;
+    impl<const AMPLITUDE: u8> std::ops::Mul<Signal> for Amplifier<AMPLITUDE> {
+        type Output = Signal;
 
-        fn mul(self, rhs: Full) -> Self::Output {
-            Full::cap(rhs.as_f32() * AMPLITUDE as f32)
-        }
-    }
-
-    impl<const AMPLITUDE: u8> std::ops::Mul<Half> for Amplifier<AMPLITUDE> {
-        type Output = Half;
-
-        fn mul(self, rhs: Half) -> Self::Output {
-            Half::cap(rhs.as_f32() * AMPLITUDE as f32)
-        }
-    }
-
-    impl<const AMPLITUDE: u8> Amplifier<AMPLITUDE> {
-        fn amplify<S: Signal>(signal: S) -> S {
-            S::cap(signal.as_f32())
+        fn mul(self, rhs: Signal) -> Self::Output {
+            Signal::cap(rhs.as_f32() * f32::from(AMPLITUDE))
         }
     }
 
@@ -97,12 +130,22 @@ mod signal {
     pub enum Aggregator {
         Linear,
         Tangential,
-        Exponential,
     }
 
     impl Aggregator {
-        pub fn aggregate(&self, inputs: impl Iterator<Item = Full>) -> Full {
-            unimplemented!();
+        pub fn aggregate(self, inputs: impl Iterator<Item = Signal>) -> Signal {
+            match self {
+                Self::Linear => {
+                    let sum = inputs
+                        .enumerate()
+                        .fold((0, 0.0), |a, c| (c.0, a.1 + c.1.as_f32()));
+                    // ALLOWED: Because there cannot be that many synapses (not enough neurons to
+                    // saturate 23 bits)
+                    #[allow(clippy::cast_precision_loss)]
+                    Signal::cap(sum.1 / sum.0 as f32)
+                }
+                Self::Tangential => Signal::cap(inputs.fold(0.0, |a, c| a + c.as_f32()).tanh()),
+            }
         }
     }
 }
