e23c02d0177a4db6349effaa5f2433edb321e20e
Marcelo Lima <marcelowind@gmail.com>
2021-11-30 14:19:47 +0100

    WIP: floater: make world floaty

--- src/sim/world.rs
+++ src/sim/world.rs
@@ -1,7 +1,7 @@
 use super::Index;
 
 pub struct World {
-    size: u8,
+    size: f32,
     coord: Vec<Coordinate>,
     // Walls
     // Foods
@@ -17,14 +17,21 @@ impl World {
         let count = count.min(u16::from(size) * u16::from(size));
 
         let coord = (0..size)
-            .flat_map(|x| (0..size).map(|y| Coordinate::new(x, y)).collect::<Vec<_>>())
+            .flat_map(|x| {
+                (0..size)
+                    .map(|y| Coordinate::new(f32::from(x), f32::from(y)))
+                    .collect::<Vec<_>>()
+            })
             .choose_multiple(&mut rand::thread_rng(), count as usize);
 
-        Self { size, coord }
+        Self {
+            size: f32::from(count),
+            coord,
+        }
     }
 
     #[inline]
-    pub fn size(&self) -> u8 {
+    pub fn size(&self) -> f32 {
         self.size
     }
 
@@ -33,24 +40,51 @@ impl World {
         unsafe { *self.coord.get_unchecked(index.0) }
     }
 
-    pub fn advance(&mut self, index: Index, direction: Direction) {
-        // SAFETY: Called from a sim step. Always within bounds
-        let coord = unsafe { self.coord.get_unchecked(index.0) };
+    #[inline]
+    fn coord_mut(&mut self, index: Index) -> &mut Coordinate {
+        unsafe { self.coord.get_unchecked_mut(index.0) }
+    }
 
-        let dest = match direction {
-            Direction::North if coord.y() > 0 => coord.neighbor(Direction::North),
-            Direction::North => return,
-            Direction::East if coord.x() < self.size - 1 => coord.neighbor(Direction::East),
-            Direction::East => return,
-            Direction::South if coord.y() < self.size - 1 => coord.neighbor(Direction::South),
-            Direction::South => return,
-            Direction::West if coord.x() > 0 => coord.neighbor(Direction::West),
-            Direction::West => return,
-        };
+    pub fn advance(&mut self, index: Index, speed: f32, direction: Direction) {
+        let coord = self.coord_mut(index);
 
-        if !self.coord.iter().enumerate().any(|(_, c)| *c == dest) {
-            // SAFETY: Previously checked for bounds
-            unsafe { *self.coord.get_unchecked_mut(index.0) = dest };
+        if let Some((neighbor, mut dist)) = self
+            .coord
+            .iter()
+            .filter(|c| c.direction(*coord).0.abs() <= std::f32::consts::PI / 4.) // Only in a cone in front
+            .filter_map(|c| {
+                let dist = c.distance(*coord);
+                if dist < speed {
+                    Some((c, dist))
+                } else {
+                    None
+                }
+            })
+            .fold(None, |acc, (cc, cd)| {
+                if let Some((ac, ad)) = acc {
+                    if ad < cd {
+                        return Some((ac, ad));
+                    }
+                }
+                Some((cc, cd))
+            })
+        {
+            while dist > 0.01 {
+                coord.translate(direction, dist);
+                dist = neighbor.distance(*coord);
+            }
+        }
+
+        if coord.x() < 0. {
+            coord.set_x(0.);
+        } else if coord.x() > self.size {
+            coord.set_x(self.size);
+        }
+
+        if coord.y() < 0. {
+            coord.set_y(0.);
+        } else if coord.y() > self.size {
+            coord.set_y(self.size);
         }
     }
 
@@ -59,51 +93,118 @@ impl World {
     }
 }
 
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
-pub enum Direction {
-    North = 0,
-    East,
-    South,
-    West,
-}
+#[derive(Debug, Copy, Clone, PartialEq, PartialOrd)]
+pub struct Direction(f32);
 
 impl Direction {
-    pub fn from(value: u8) -> Self {
-        match value & 0b11 {
-            3 => Self::West,
-            2 => Self::South,
-            1 => Self::East,
-            _ => Self::North,
+    pub fn new(rads: f32) -> Self {
+        Self(rads)
+    }
+
+    fn desaturate(&mut self) {
+        if self.0 > std::f32::consts::TAU {
+            self.0 -= std::f32::consts::TAU;
+            while self.0 > std::f32::consts::TAU {
+                self.0 -= std::f32::consts::TAU;
+            }
+        } else {
+            while self.0 < 0. {
+                self.0 += std::f32::consts::TAU;
+            }
         }
+    }
+
+    pub fn as_rad(&self) -> f32 {
+        self.0
     }
 }
 
-#[derive(Debug, Copy, Clone, Eq, PartialEq, Ord, PartialOrd)]
-pub struct Coordinate(u16);
+impl std::ops::AddAssign<f32> for Direction {
+    fn add_assign(&mut self, rhs: f32) {
+        self.0 += rhs;
+        self.desaturate();
+    }
+}
+
+impl std::ops::SubAssign<f32> for Direction {
+    fn sub_assign(&mut self, rhs: f32) {
+        self.0 -= rhs;
+        self.desaturate();
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, PartialOrd)]
+pub struct Coordinate(f32, f32);
 
 impl Coordinate {
-    pub fn new(x: u8, y: u8) -> Self {
-        Self(u16::from(x) | (u16::from(y) << 8))
+    pub fn new(x: f32, y: f32) -> Self {
+        Self(x, y)
     }
 
-    // ALLOWED: There is a bit mask already limiting the result
-    #[allow(clippy::cast_possible_truncation)]
-    pub fn x(self) -> u8 {
-        (self.0 & 0xff) as u8
+    pub fn x(self) -> f32 {
+        self.0
     }
 
-    // ALLOWED: There is a bit mask already limiting the result
-    #[allow(clippy::cast_possible_truncation)]
-    pub fn y(self) -> u8 {
-        (self.0 >> 8 & 0xff) as u8
+    pub fn set_x(&mut self, x: f32) {
+        self.0 = x;
     }
 
-    pub fn neighbor(self, direction: Direction) -> Self {
-        match direction {
-            Direction::North => Self(self.0 - 0x100),
-            Direction::East => Self(self.0 + 1),
-            Direction::South => Self(self.0 + 0x100),
-            Direction::West => Self(self.0 - 1),
-        }
+    pub fn y(self) -> f32 {
+        self.1
+    }
+
+    pub fn set_y(&mut self, y: f32) {
+        self.0 = y;
+    }
+
+    pub fn max(self) -> f32 {
+        self.0.max(self.1)
+    }
+
+    pub fn min(self) -> f32 {
+        self.0.min(self.1)
+    }
+
+    pub fn abs(self) -> Self {
+        Self(self.0.abs(), self.1.abs())
+    }
+
+    pub fn direction(self, rhs: Self) -> Direction {
+        let vec = self - rhs;
+        Direction(vec.0.atan2(vec.1))
+    }
+
+    pub fn translate(&mut self, dir: Direction, amount: f32) {
+        self.0 *= dir.0.cos() * amount;
+        self.1 *= dir.0.sin() * amount;
+    }
+
+    pub fn distance(self, rhs: Self) -> f32 {
+        let manhattan = self - rhs;
+        (manhattan.0 * manhattan.0 + manhattan.1 * manhattan.1).sqrt()
+    }
+}
+
+// TODO: Remove if uneeded
+// impl std::ops::Add for Coordinate {
+//     type Output = Self;
+
+//     fn add(self, rhs: Self) -> Self::Output {
+//         Self(self.0 + rhs.0, self.1 + rhs.1)
+//     }
+// }
+
+// impl std::ops::AddAssign for Coordinate {
+//     fn add_assign(&mut self, rhs: Self) {
+//         self.0 += rhs.0;
+//         self.1 += rhs.1;
+//     }
+// }
+
+impl std::ops::Sub for Coordinate {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self(self.0 - rhs.0, self.1 - rhs.1)
     }
 }
